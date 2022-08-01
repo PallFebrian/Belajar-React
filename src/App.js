cf8b57ff35c282eed6860055940910e35b43b395
PallFebrian <pallfebrian9@gmail.com>
2022-08-01 15:01:18 +0700

    latihan1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
 import React from "react";
+import Produk from "./latihan1/latihan1";
 
-function App() {
-  return (
+
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <h1>Latihan Export Import</h1>
+//     </React.Fragment>
+//   );
+// }
+
+// // export nama bisa 1 komponen atau lebih
+
+// export default App;
+
+function App(){
+  let [produk,setProduk] = React.useState([
+    {
+      jenis:"Elektronik",
+      produlsi:"handphone",
+      brand: [
+        {
+          nama:"samsung",
+          harga:'Rp.1.000.000',
+        },
+        {
+          nama:"xiaomi",
+          harga:'Rp.500.000'
+        },
+      ],
+    },
+    {
+      jenis:"Tranportasi",
+      produlsi:"Mobil",
+      brand: [
+        {
+          nama:"Toyota",
+          harga:'Rp.1.000.000,000',
+        },
+        {
+          nama:"xiaomi",
+          harga:'Rp.500.000.00'
+        }
+      ]
+
+    }
+  ])
+
+  return(
     <React.Fragment>
-      <h1>Latihan Export Import</h1>
+      <h1>latihan 01</h1>
+      <produk data = {produk}/>
     </React.Fragment>
-  );
+  )
+
 }
 
-// export nama bisa 1 komponen atau lebih
 
-export default App;
+export default Produk
---
+++ src/latihan1/latihan1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function Produk({data}){
+    return(
+        <React.Fragment>
+            Latihan 1
+            {Produk?.map(function(data,index)}
+        </React.Fragment>
+    )
+}

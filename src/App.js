517f0a72e0899269a32a86ebe749bb59ea7132b9
PallFebrian <pallfebrian9@gmail.com>
2022-08-04 14:29:53 +0700

    belajar-handle-change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React from "react";
+import Button from "./komponen/button";
+import Layout from "./komponen/layout";
+import "./style/style.css";
 
-function App() {
-  return (
+export default function App(){
+  
+  let [name, setName] = React.useState()
+  let [email, setEmail] = React.useState()
+  let [password, setPassword] = React.useState()
+  let [confirmPassword, setConfirmPassword] = React.useState()
+  return(
     <React.Fragment>
-      <h1>Latihan Export Import</h1>
+      <form>TES</form>
     </React.Fragment>
-  );
+  )
 }
 
-// export nama bisa 1 komponen atau lebih
 
-export default App;
+// function App() {
+//   let [count, setCount] = React.useState(0);
+//   const handleTambah = () => {
+//     setCount(count + 1);
+//   };
+//   const handleKurang = () => {
+//     setCount(count - 1);
+//   };
+//   return (
+//     <React.Fragment>
+//       <h1>Count = {count}</h1>
+//       <Button onClick={handleTambah} title="tambah" color="blue" />
+//       <Button
+//         disabled={count === 0 ? true : false}
+//         onClick={handleKurang}
+//         title="kurang"
+//         color="green"
+//       />
+//       <Button
+//         disabled={count === 0 ? true : false}
+//         onClick={() => {
+//           setCount(0);
+//         }}
+//         title="reset"
+//       />
+//     </React.Fragment>
+//   );
+// }
+
+// // export nama bisa 1 komponen atau lebih
+
+// export default App;
---
+++ src/komponen/button.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+// export default function Button({
+//     title,
+//     color = "red",
+//     onClick,
+//     name,
+//     id,
+//     disable = false,
+//   }) {
+//     return (
+//       <React.Fragment>
+//         <button
+//           onClick={onClick}
+//           nama={name}
+//           id={id}
+//           disabled={disable}
+//           style={{
+//             backgroundColor: color,
+//           }}
+//           className="button"
+//         >
+//           {title}
+//         </button>
+//       </React.Fragment>
+//     );
+//   }
+
+export default function Button({ title, color = "red", disabled, ...props }) {
+  return (
+    <React.Fragment>
+      <button
+        disabled={disabled}
+        {...props}
+        style={{
+          backgroundColor: color,
+          opacity: disabled ? 0.5 : 1,
+        }}
+        className="button"
+      >
+        {title}
+      </button>
+    </React.Fragment>
+  );
+}

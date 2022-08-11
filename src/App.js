9102400414b86428acc901560fef2fe1ab07daad
PallFebrian <pallfebrian9@gmail.com>
2022-08-11 14:29:18 +0700

    belajar useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function App() {
+  let [count, setCount] = React.useState(0);
+  let [text, setText] = React.useState(true);
+  let [message, setMessage] = React.useState(0);
+
+  React.useEffect(() => {
+    setMessage(message + 1);
+    console.log("useEffect berjalan");
+  }, [count, text]);
+  console.log(text);
+
+  let [isLoading, setIsLoading] = React.useState(true);
+  React.useEffect(() => {
+    setTimeout(()=>{
+      setIsLoading(false)
+    },2000)
+  }, []);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <React.Fragment>
-      <h1>Latihan Export Import</h1>
+      <h1>Belajar useEffect</h1>
+      <h3>{message === 10 ? "ini sepuluh" : "ini bukan sepuluh"}</h3>
+      <h1>message{message}</h1>
+      <h1>Count{count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Tambah
+      </button>
+      <button
+        onClick={() => {
+          setText(!text);
+        }}
+      >
+        Ubah
+      </button>
     </React.Fragment>
   );
 }

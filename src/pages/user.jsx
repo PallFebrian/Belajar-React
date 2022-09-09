dd1698aeb979dd48d1e0dcb57bb678b4da8e2d6b
PallFebrian <pallfebrian9@gmail.com>
2022-09-09 14:17:25 +0700

    tugas-alquran

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import axios from "axios";
-export default function User() {
-  const [users, setUsers] = React.useState([]); // state  untuk menyimpan data user dari API
+export default function Surah() {
+  const [surah, setSurah] = React.useState([]); // state  untuk menyimpan data user dari API
   const [page, setPage] = React.useState(1);
 
   const getUserHandle = async () => {
     try {
       const response = await axios.get(
-        `https://reqres.in/api/users?page=${page}`
+        `https://equran.id/api/surat`
       );
-      console.log("response =>", response.data);
-      setUsers(response.data.data);
-      setPage(response.data.page);
+      console.log("response =>", response);
+      setSurah(response.data);
+      setPage(response.data);
     } catch (err) {}
   };
 
-  console.log("user =>", users);
+  console.log("surah =>", surah);
   console.log("page  =>", page);
 
   React.useEffect(() => {
@@ -23,59 +23,34 @@ export default function User() {
   }, [page]);
 
   return (
-    <div>
+    <div className="bg-midnight text-tahiti">
       <h1>Tabel User</h1>
       <table className="table-auto">
         <thead>
           <tr className="text-left border">
             <th className="">No</th>
-            <th>Email</th>
-            <th>Frist Name</th>
-            <th>Last Name</th>
-            <th>Avatar</th>
-            <th>Detail</th>
+            <th>Nama</th>
+            <th>Nama Latin</th>
+            <th>Jumlah Ayat</th>
+            <th>Tempat Turun</th>
+            <th>Arti</th>
           </tr>
         </thead>
         <tbody>
-          {users.map((user, index) => {
+          {surah.map((surah, index) => {
             return (
               <tr key={index} className="text-left border">
                 <td>{index + 1}</td>
-                <td>{user.email}</td>
-                <td>{user.first_name}</td>
-                <td>{user.last_name}</td>
-                <td>
-                  <img
-                    className="rounded-full h-5 w-5"
-                    src={user.avatar}
-                    alt={users.avatar}
-                  />
-                </td>
-                <td>Detail</td>
+                <td>{surah.nama}</td>
+                <td>{surah.nama_latin}</td>
+                <td>{surah.jumlah_ayat}</td>
+                <td>{surah.tempat_turun}</td>
+                <td>{surah.arti}</td>
               </tr>
             );
           })}
         </tbody>
       </table>
-
-      <p>saat ini di table {page}</p>
-
-      <div className="flex items-center justify-center ">
-        <button
-          onClick={() => {
-            setPage(page - 1);
-          }}
-        >
-          Previos
-        </button>
-        <button
-          onClick={() => {
-            setPage(page + 1);
-          }}
-        >
-          Next
-        </button>
-      </div>
     </div>
   );
 }
